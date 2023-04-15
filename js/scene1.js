3b1de2759f3337b5f08b1ffff9468e4be484871f
unoptimal <unoptimalblogs@gmail.com>
2023-04-15 12:37:43 -0700

    new stuff

---
+++ js/scene1.js
@@ -0,0 +1,4 @@
+const scene1NextBtn = document.getElementById('scene1next');
+  scene1NextBtn.addEventListener('click', function() {
+    console.log('Hi! The scene1.js file is linked correctly.');
+  });
